78a02565ae9cf0c6d1d8c23587c415dfd472fbad
Feli87 <hckt87@gmail.com>
2024-02-12 11:16:10 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,11 @@ export default async function Home() {
           <div className="max-w-exl mx-auto text-center">
             <div>
             <span className="w-auto px-6 py-3">
-              <span className="text-sm font-bold text-primary rounded-full bg-secondary px-4 py-3 ">Sort your notes easily</span>
+              <span className="text-sm font-bold text-primary rounded-full bg-secondary px-4 py-3 ">Demo SaaS App</span>
             </span>
             <h1 className="text-5xl font-bold my-4">Create Notes With Ease</h1>
             <p className="max-w-xl mx-auto text-lg mt-4 text-secondary-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore corporis cupiditate quasi aspernatur ad ullam veniam.
+            Elevate your productivity with our intuitive task management app. Say goodbye to chaos and hello to efficiency.
             </p>
             </div>
             <div className="flex items-center justify-center max-2-xm mx-auto mt-8">
